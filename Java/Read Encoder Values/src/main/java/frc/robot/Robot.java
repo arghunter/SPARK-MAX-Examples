baa51537d73381bcaa87430dce120b9fe1047f23
arghunter <acgo8888@gmail.com>
2022-10-19 17:19:44 -0700

    canbustester

--- Java/Read Encoder Values/src/main/java/frc/robot/Robot.java
+++ Java/Read Encoder Values/src/main/java/frc/robot/Robot.java
@@ -16,44 +16,78 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
-  private Joystick m_stick;
+  // private Joystick m_stick;
   private static final int deviceID = 1;
-  private CANSparkMax m_motor;
-  private RelativeEncoder m_encoder;
+  private CANSparkMax m1_motor;
+  private CANSparkMax m2_motor;
+  private CANSparkMax m3_motor;
+  private CANSparkMax m4_motor;
+
+  private RelativeEncoder m1_encoder;
+
+  private RelativeEncoder m2_encoder;
+  private RelativeEncoder m3_encoder;
+
+  private RelativeEncoder m4_encoder;
 
   @Override
   public void robotInit() {
     // initialize SPARK MAX
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    m1_motor = new CANSparkMax(22, MotorType.kBrushless);
+    m2_motor = new CANSparkMax(20, MotorType.kBrushless);
+    m3_motor = new CANSparkMax(21, MotorType.kBrushless);
+    m4_motor = new CANSparkMax(24, MotorType.kBrushless);
+    // m5_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
 
     /**
      * The RestoreFactoryDefaults method can be used to reset the configuration parameters
      * in the SPARK MAX to their factory default state. If no argument is passed, these
      * parameters will not persist between power cycles
      */
-    m_motor.restoreFactoryDefaults();
+    m1_motor.restoreFactoryDefaults();
+
+    m2_motor.restoreFactoryDefaults();
+
+    m3_motor.restoreFactoryDefaults();
+
+    m4_motor.restoreFactoryDefaults();
+    // m5_motor.restoreFactoryDefaults();
 
     /**
     * In order to read encoder values an encoder object is created using the 
     * getEncoder() method from an existing CANSparkMax object
     */
-    m_encoder = m_motor.getEncoder();
+    // m1_encoder = m1_motor.getEncoder();
+    // m2_encoder = m2_motor.getEncoder();
 
-    m_stick = new Joystick(0);
+    // m3_encoder = m3_motor.getEncoder();
+
+    // m4_encoder = m4_motor.getEncoder();
+    // m5_encoder = m5_motor.getEncoder();
+
+
+    // m_stick = new Joystick(0);
   }
 
   @Override
   public void teleopPeriodic() {
     // set the motor output based on jostick position
-    m_motor.set(m_stick.getY());
+    m1_motor.set(0.2);
 
+    m2_motor.set(0.2);
+
+    m3_motor.set(0.2);
+
+    m4_motor.set(0.2);
+
+    
     /**
      * Encoder position is read from a RelativeEncoder object by calling the
      * GetPosition() method.
      * 
      * GetPosition() returns the position of the encoder in units of revolutions
      */
-    SmartDashboard.putNumber("Encoder Position", m_encoder.getPosition());
+    // SmartDashboard.putNumber("Encoder Position", m_encoder.getPosition());
 
     /**
      * Encoder velocity is read from a RelativeEncoder object by calling the
@@ -61,6 +95,6 @@ public class Robot extends TimedRobot {
      * 
      * GetVelocity() returns the velocity of the encoder in units of RPM
      */
-    SmartDashboard.putNumber("Encoder Velocity", m_encoder.getVelocity());
+    // SmartDashboard.putNumber("Encoder Velocity", m_encoder.getVelocity());
   }
 }
